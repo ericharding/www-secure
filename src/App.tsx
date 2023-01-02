b45d5cb94d0bc69a4aa8b26d65a6dfb16b8da64d
Eric Harding <eric@digitalsorcery.net>
2023-01-02 13:08:19 -0500

    Input stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
-import type { Component } from 'solid-js';
+import { Component, createEffect, createSignal, ParentComponent } from "solid-js";
+
+const Label: ParentComponent<{ text: string }> = (props) => {
+  return (
+    <label class="bg-slate-300 flex flex-row w-96">
+      <span class="text-black basis-1/4">{props.text}</span>
+      {props.children}
+    </label>
+  );
+};
 
 const App: Component = () => {
+  const [password, setPassword] = createSignal("");
+  const [key, setKey] = createSignal("");
+  const inputChanged = (fn) => (e) => {
+    const target = e.target as HTMLInputElement;
+    fn(target?.value ?? "");
+  };
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+    <div class="flex flex-col items-center justify-center p-4 bg-slate-400">
+      <Label text="Password:">
+        <input type="password" class="grow" value={password()} onChange={inputChanged(setPassword)} />
+      </Label>
+      <Label text="URL:">
+        <input type="text" class="grow" value={password()} onChange={inputChanged(setKey)} />
+      </Label>
+    </div>
   );
 };
 
