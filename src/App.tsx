57b561388381e38ad54ccf80d7a5bbf0c9e15fa1
Eric Harding <eric@digitalsorcery.net>
2023-01-02 16:57:38 -0500

    Add copy to clipboard button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { Component, createEffect, createSignal, onCleanup, onMount, ParentComponent } from 'solid-js'
 import gen from './hasher'
 
+const CopyButton: Component<{ value?: string }> = (props) => {
+  return (
+    <input
+      type="button"
+      class="py-0 px-2 grow-0 border-2 bg-transparent hover:bg-slate-700 active:bg-orange-800 active:scale-95 text-white font-bold rounded"
+      value="📋"
+      onClick={(e) => navigator.clipboard.writeText(props.value)}
+    />
+  )
+}
+
 const Label: ParentComponent<{ text: string }> = (props) => {
   return (
-    <label class="bg-slate-300 flex flex-row w-96">
-      <span class="text-black basis-1/3 px-1">{props.text}</span>
+    <label class="bg-slate-300 flex flex-row w-1/3">
+      <span class="text-black basis-1/3 grow-0 mx-1">{props.text}</span>
       {props.children}
     </label>
   )
@@ -14,6 +25,7 @@ const ClipText: Component<{ label: string; value?: string }> = (props) => {
   return (
     <Label text={props.label}>
       <input type="text" readonly value={props.value ?? ''} class="grow bg-gray-50" />
+      <CopyButton value={props.value} />
     </Label>
   )
 }
@@ -64,13 +76,7 @@ const App: Component = () => {
     <>
       <div class="flex flex-col items-center justify-center p-4 bg-slate-400">
         <Label text="Password:">
-          <input
-            autofocus
-            type="password"
-            class="grow"
-            value={password()}
-            onChange={inputChanged(setPassword)}
-          />
+          <input autofocus type="password" class="grow" value={password()} onChange={inputChanged(setPassword)} />
         </Label>
         <Label text="Token:">
           <input type="text" class="grow" value={token()} onChange={inputChanged(setToken)} />
