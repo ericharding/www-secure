53b7ced8d3260d7ae27bb5bc52efdee4d777002f
Eric Harding <eric@digitalsorcery.net>
2023-01-02 13:17:28 -0500

    Layout done

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,16 @@ import { Component, createEffect, createSignal, ParentComponent } from "solid-js
 const Label: ParentComponent<{ text: string }> = (props) => {
   return (
     <label class="bg-slate-300 flex flex-row w-96">
-      <span class="text-black basis-1/4">{props.text}</span>
+      <span class="text-black basis-1/3 px-1">{props.text}</span>
       {props.children}
     </label>
   );
+};
+
+const ClipText: Component<{ label: string }> = (props) => {
+  return <Label text={props.label}>
+    <input type="text" readonly class="grow bg-gray-50" />
+  </Label>;
 };
 
 const App: Component = () => {
@@ -17,14 +23,21 @@ const App: Component = () => {
     fn(target?.value ?? "");
   };
   return (
-    <div class="flex flex-col items-center justify-center p-4 bg-slate-400">
-      <Label text="Password:">
-        <input type="password" class="grow" value={password()} onChange={inputChanged(setPassword)} />
-      </Label>
-      <Label text="URL:">
-        <input type="text" class="grow" value={password()} onChange={inputChanged(setKey)} />
-      </Label>
-    </div>
+    <>
+      <div class="flex flex-col items-center justify-center p-4 bg-slate-400">
+        <Label text="Password:">
+          <input type="password" class="grow" value={password()} onChange={inputChanged(setPassword)} />
+        </Label>
+        <Label text="Token:">
+          <input type="text" class="grow" value={password()} onChange={inputChanged(setKey)} />
+        </Label>
+      </div>
+      <div class="flex flex-col items-center justify-center p-4 bg-slate-500">
+        <ClipText label="Result" />
+        <ClipText label="Short (8):" />
+        <ClipText label="Medium (12):" />
+      </div>
+    </>
   );
 };
 
